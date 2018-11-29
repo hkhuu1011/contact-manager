a5ab696e2ffe92e0f82717bae0cc6454ae47f740
Han Khuu <h.khuu01@gmail.com>
2018-11-29 17:31:41 -0500

    routes working

--- angular-module/src/app/app-routing.module.ts
+++ angular-module/src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+
 
 const appRoutes: Routes = [
 	{
 		path: 'login',
 		component: LoginComponent
+	},
+	{
+		path: 'signup',
+		component: SignupComponent
+	},
+	{
+		path: 'profile',
+		component: ProfileComponent
+	},
+	{
+		path: 'request-password-reset',
+		component: RequestResetComponent
+	},
+	{
+		path: 'response-password-reset',
+		component: ResponseResetComponent
 	},
 ];
 
@@ -13,6 +34,6 @@ const appRoutes: Routes = [
   imports: [
     RouterModule.forRoot(appRoutes)
   ],
-  declarations: []
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
