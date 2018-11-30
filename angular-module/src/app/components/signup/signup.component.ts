6017cbeb270e6790c834dfa497ecb5947b9ea8c7
Han Khuu <h.khuu01@gmail.com>
2018-11-29 21:34:03 -0500

    working on displaying errors when sign up credentials are not valid

--- angular-module/src/app/components/signup/signup.component.ts
+++ angular-module/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  public form = {
+  	email: null,
+  	name: null,
+  	password: null,
+  	password_confirmation: null
+  };
+
+  public error = [];
+
+  constructor(private http: HttpClient) { }
+
+  onSubmit() {
+  	return this.http.post('http://localhost:8000/api/signup', this.form).subscribe(
+  		data => console.log(data),
+  		error => this.handleError(error)
+  	);
+  }
+
+  handleError(error) {
+  	this.error = error.error.errors;
+  }
 
   ngOnInit() {
   }
