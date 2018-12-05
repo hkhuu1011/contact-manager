feddeeb66b14f3162561d2c43bfd90f79fbeb02c
Han Khuu <h.khuu01@gmail.com>
2018-12-04 19:09:20 -0500

    logout button is working and removes token

--- angular-module/src/app/components/navbar/navbar.component.ts
+++ angular-module/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AuthService } from '../../services/auth.service';
+import { TokenService } from '../../services/token.service';
 
 @Component({
   selector: 'app-navbar',
@@ -12,11 +14,21 @@ export class NavbarComponent implements OnInit {
   public loggedIn : boolean;
 
   constructor(
-  	private Auth : AuthService
+  	private Auth : AuthService,
+  	private router: Router,
+  	private Token: TokenService
   ) { }
 
   ngOnInit() {
   	this.Auth.authStatus.subscribe(value => this.loggedIn = value);
+
+  }
+
+  logout(event: MouseEvent) {
+  	event.preventDefault();
+  	this.Token.remove();
+  	this.Auth.changeAuthStatus(false);
+    this.router.navigateByUrl('/login');
 
   }
 
